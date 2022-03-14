042462d51d9f951c8d4d73f3971f192f995c86ae
limnengdu <98989546+limnengdu@users.noreply.github.com>
2022-03-14 14:33:16 -0700

    Update pump_and_ultrasoundv5.ino

--- Code examples/pump_and_ultrasoundv5/pump_and_ultrasoundv5.ino
+++ Code examples/pump_and_ultrasoundv5/pump_and_ultrasoundv5.ino
@@ -2,11 +2,12 @@
 #define echoPin 33
 #define relayPin 12
 float duration, distance, time_required;
-int final_distance = 2; //TBD
-int flow_rate = 5; //litres per second (to be determined)
-int surface_area = 4; // (to be determined)
-int height_rate = flow_rate/surface_area; //rate of height increase
+float final_distance = 7; // 5(ultra to cup top) + 2 (buffer)
+float flow_rate = 22.2; //cm^3 per second (to be determined)
+float surface_area = 43; // (to be determined)
+float height_rate = flow_rate/surface_area; //rate of height increase
 int count = 0; //counter for millis
+float max_time = 20; //maximum pump run time
 //int buffer_time = 2;// time require for water to reach tip of tube from tank(seconds)
 long previousMillis = 0;
 
@@ -20,13 +21,17 @@ void setup() {
 }
 
 void loop(){
-  measure_dispense();)
-
+  measure_dispense();
+  delay(5000);
+}
 void measure_dispense() {
   // put your main code here, to run repeatedly:
   calc_distance();
   Serial.println(distance);
   time_required = (distance-final_distance)/height_rate;
+  if (time_required > max_time){
+    time_required = max_time;
+  }
   Serial.println(time_required);
 
   long currentMillis = millis();
