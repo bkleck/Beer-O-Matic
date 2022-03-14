44d610eb3ab786327b2346e6095dbaebd51029f9
Boon Kong <kongleck@yahoo.com.sg>
2022-03-14 15:58:59 -0700

    read pins work

--- Code examples/full_logic/full_logic.ino
+++ Code examples/full_logic/full_logic.ino
@@ -1,19 +1,3 @@
-/*
- Standard configurations for the prodriver library
-
-  ARDUINO --> PRODRIVER
-  D8 --> STBY
-  D7 --> EN
-  D6 --> MODE0
-  D5 --> MODE1
-  D4 --> MODE2
-  D3 --> MODE3
-  D2 --> ERR
-
-   - if need to change, use myProDriver.settings.standbyPin = A0;
-   - follow example 10 in library
- */
-
 #include "SparkFun_ProDriver_TC78H670FTG_Arduino_Library.h"
 #include <ezButton.h>
 PRODRIVER myProDriver; //Create instance of driver
@@ -21,15 +5,14 @@ PRODRIVER myProDriver; //Create instance of driver
 const int leftLimit = 12;
 const int rightLimit = 11;
 const int IR1 = A0;
-const int button1 = A1;
+const int Button1 = A1;
 
 bool limit;
 int dir = 0;
 int init_light_reading;
-int light_reading;
 
-char currentPos;
-char targetPos;
+String currentPos;
+String targetPos;
 const char *sequence[5] = {"LEFT", "1", "2", "3", "RIGHT"};
 
 /* function to call when the circuit is first turned on
@@ -54,9 +37,37 @@ bool ReadPin(int pinNumber) {
   return result;
 }
 
+// function to set the target position whenever a button is read HIGH
 char ReadTarget() {
-  if ReadPin(A1) {
+  if (ReadPin(Button1) == LOW) {
     targetPos = "1";
+  }
+}
+
+// function to set the current position whenever a limit switch or IR sensor is activated
+char ReadCurrent() {
+  if (ReadPin(leftLimit) == LOW) {
+    currentPos = "LEFT";
+  }
+  
+  else if (ReadPin(rightLimit) == LOW) {
+    currentPos = "RIGHT";
+  }
+
+  else if (ReadIRSensor(IR1) == LOW) {
+    currentPos = "1";
+  }
+}
+
+
+bool ReadIRSensor(int IR_pin) {
+  int light_reading = analogRead(IR1);
+  if (light_reading <= init_light_reading - 100) {
+    Serial.println("Something fat passed by: " + IR_pin);
+    return 0;
+  }
+  else {
+    return 1;
   }
 }
 
@@ -65,20 +76,27 @@ void setup() {
   Serial.begin(115200);
   pinMode(12, INPUT_PULLUP);
   pinMode(11, INPUT_PULLUP);
+  pinMode(A1, INPUT_PULLUP);
   myProDriver.begin();
   init_light_reading = analogRead(IR1);
-  Startup();
+  // Startup();
 }
 
 void loop() {
-  if (ReadPin(rightLimit)) {
-    myProDriver.step(1, dir);
+  Serial.println(ReadPin(Button1));
 
-    light_reading = analogRead(IR1);
-    if (light_reading <= init_light_reading - 100) {
-      Serial.println("Something fat passed by");
-    }
+  ReadTarget();
+  ReadCurrent();
+  Serial.print("current position: " ); 
+  Serial.println(currentPos);
+  Serial.print("target position: "); 
+  Serial.println(targetPos);
+  delay(1000);
+  if (targetPos) {
+    
   }
+
+  
 
   else {
     Serial.println("reached right limit");
