3d7e688bf7910b923e1ca966f8ef83a34f60c4d9
Boon Kong <kongleck@yahoo.com.sg>
2022-04-04 15:20:13 -0700

    test 1

---
+++ Code examples/move_left/move_left.ino
@@ -0,0 +1,52 @@
+#include "SparkFun_ProDriver_TC78H670FTG_Arduino_Library.h"
+PRODRIVER myProDriver; //Create instance of driver
+
+const int leftLimit = 12;
+const int rightLimit = 11;
+const int IR1 = A0;
+int dir = 0;
+int init_light_reading;
+
+// function to read digital pins, returning 1 or 0
+bool ReadPin(int pinNumber) {
+  bool result = digitalRead(pinNumber);
+  return result;
+}
+
+void Startup() {
+  while (ReadPin(leftLimit)) {
+    myProDriver.step(200, dir);
+  }
+
+  Serial.println("reached left limit");
+  dir = dir + 1;
+  delay(3000);
+}
+
+bool ReadIRSensor(int IR_pin) {
+  int light_reading = analogRead(IR_pin);
+  if (light_reading <= init_light_reading - 100) {
+    Serial.println("Something fat passed by: " + IR_pin);
+    return 0;
+  }
+  else {
+    return 1;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(12, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+  myProDriver.begin();
+  Startup();
+}
+
+void loop() {
+  if (ReadPin(rightLimit)) {
+    if (ReadIRSensor(IR1) == LOW) {
+      myProDriver.step(200, dir);
+    }
+  }
+  
+}
